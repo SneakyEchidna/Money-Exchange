17f8ff03ff2e9006e819a083729f314956a24ab0
Kanstantsin Kiyko <xfozzyx@gmail.com>
2018-02-21 14:48:20 +0300

    all tests pass

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+  const split = (value) => {
+    const coins = [50, 25, 10, 5, 1];
+    const coinsSymbols = ['H', 'Q', 'D', 'N', 'P'];
+    let splited = [];
+    let current = value;
+    let change = {};
+    coins.map((e) => {
+      splited.push(Math.floor(current / e));
+      current = current % e;
+    });
+    splited.map((e, i) => {
+      if (e !== 0) {
+        change[coinsSymbols[i]] = splited[i];
+      }
+    });
+    return change;
+  };
+  if (currency <= 0) {
+    return {};
+  }
+  if (currency > 10000) {
+    return {
+      error:
+        "You are rich, my friend! We don't have so much coins for exchange",
+    };
+  }
+  return split(currency);
+};
